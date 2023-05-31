73833aa9ef8975d9e16c021d32c63abb908bb1af
UtfCube <black_fox_0@mail.ru>
2023-05-31 18:47:38 +0300

    add update text fields handler

--- src/Handler.php
+++ src/Handler.php
@@ -16,9 +16,11 @@ use Manticoresearch\Buddy\Core\Task\Column;
 use Manticoresearch\Buddy\Core\Task\Task;
 use Manticoresearch\Buddy\Core\Task\TaskResult;
 use Manticoresearch\Buddy\Core\ManticoreSearch\Endpoint as ManticoreEndpoint;
+use Manticoresearch\Buddy\Plugin\UpdateText\QueryParser\Datatype;
 use RuntimeException;
 use parallel\Runtime;
 use Exception;
+use Manticoresearch\Buddy\Plugin\UpdateText\QueryParser\Datalim;
 
 class Handler extends BaseHandlerWithClient {
 	/**
@@ -37,35 +39,87 @@ class Handler extends BaseHandlerWithClient {
 	 * @throws RuntimeException
 	 */
 	public function run(Runtime $runtime): Task {
-		// print_r($this->payload->parsed);
-		// $parsed = $this->payload->parsed;
-		// $table = $parsed["UPDATE"][0]["table"];
-		// $where = $parsed["WHERE"];
-		// $whereStr = implode(' ', array_map(fn($x) => $x['base_expr'], $where));
-		// $repls = ['%NAME%' => $table, '%WHERE_EXPR%' => $whereStr];
-		// $selectQ = strtr('SELECT * FROM %NAME% WHERE %WHERE_EXPR%', $repls);
-
-		// print_r($resp);
-
-		// TODO: your logic goes into closure and should return TaskResult as response
 		$taskFn = static function (Payload $handler, HTTPClient $manticoreClient): TaskResult {
 			$parsed = $handler->parsed;
 			$table = $parsed["UPDATE"][0]["table"];
-			$where = $parsed["WHERE"];
-			$whereStr = implode(' ', array_map(fn($x) => $x['base_expr'], $where));
-			$repls = ['%NAME%' => $table, '%WHERE_EXPR%' => $whereStr];
-			$selectQ = strtr('SELECT * FROM %NAME% WHERE %WHERE_EXPR%', $repls);
 			$manticoreClient->setPath('sql?mode=raw');
-
-			$resp = $manticoreClient->sendRequest($selectQ);
+			$set = $parsed["SET"];
+			$resp = $manticoreClient->sendRequest("DESC $table");
 			if (!isset($resp)) {
 				throw new Exception('Empty queries to process');
 			}
-			print_r(json_decode($resp->getBody(), true));
-			return TaskResult::raw((array)json_decode($resp->getBody(), true));
-			// return TaskResult::withRow([
-			// 	'updatetext' => 'updatetext',
-			// ])->column('updatetext', Column::String);
+			$columnsInfo = ((array)json_decode($resp->getBody(), true))[0]['data'];
+			$columns = array_map(fn($x) => $x['Field'], $columnsInfo);
+			$setFields = array_map(fn($x) => $x['sub_tree'][0]['base_expr'], $set);
+			$notExistsColumns = array_values(array_diff($setFields, $columns));
+			if (count($notExistsColumns) > 0) {
+				throw new Exception("index $table: attribute '$notExistsColumns[0]' not found");
+			}
+			// TODO: add check valid json
+			// TODO: maybe add set fields type check 
+			$setFieldsValues = array_map(function ($x) {
+				$subTree = $x['sub_tree'];
+				return ['field' => $subTree[0]['base_expr'], 'value' => $subTree[2]['base_expr']];
+			}, $set);
+
+			$where = $parsed["WHERE"];
+			$whereStr = implode(' ', array_map(fn($x) => $x['base_expr'], $where));
+			$selectQueryBase = "SELECT * FROM $table WHERE $whereStr";
+			$columnsStr = implode(',', $columns);
+			
+			$limit = 1000;
+			$i = 0;
+			$total = 0;
+			do {
+				$offset = $i * $limit;
+				$maxMatches = ($i + 1) * $limit;
+				$selectQ = "$selectQueryBase ORDER BY id ASC LIMIT $limit OFFSET $offset OPTION max_matches=$maxMatches";
+				$resp = $manticoreClient->sendRequest($selectQ);
+				if (!isset($resp)) {
+					throw new Exception('Empty queries to process');
+				}
+				$chunk = ((array)json_decode($resp->getBody(), true))[0];
+				$rowsChunk = $chunk['data'];
+				$rowsCount = count($rowsChunk);
+				$total += $rowsCount;
+				if ($rowsCount > 0) {
+					$rowsStr = array_map(function ($row) use ($setFieldsValues, $columnsInfo) {
+						foreach ($setFieldsValues as $newValue) {
+							$row[$newValue['field']] = $newValue['value'];
+						}
+
+						$escapedRow = array_map(function ($column) use ($row) {
+							$value = $row[$column['Field']];
+							$value = match ($column['Type']) {
+								'text', 'string', 'json' => str_starts_with($value, "'") ? $value : "'$value'",
+								'mva', 'mva64' => str_starts_with($value, "(") ? $value : "($value)",
+								default => $value,
+							};
+							return $value;
+						}, $columnsInfo);
+
+						$rowStr = implode(',', $escapedRow);
+						return "($rowStr)";
+					}, $rowsChunk);
+
+					$valuesStr = implode(',', $rowsStr);
+					$replaceQ = "REPLACE INTO $table ($columnsStr) VALUES $valuesStr";
+					$resp = $manticoreClient->sendRequest($replaceQ);
+					if (!isset($resp)) {
+						throw new Exception('Empty queries to process');
+					}
+					print_r(json_decode($resp->getBody(), true));
+					if ($rowsCount < $limit) {
+						break;
+					}
+					$i += 1;
+				}
+				else {
+					break;
+				}
+			} while($i < Datalim::MySqlMaxInt);
+
+			return TaskResult::withTotal($total);
 		};
 
 		return Task::createInRuntime(
